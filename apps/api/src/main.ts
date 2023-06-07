8b377132a0e68b72791b565af48e234ad421964a
Karthick Ragavendran <karthickragavendran@Karthicks-Mac-Studio.local>
2023-06-08 04:41:21 +0530

    add github workflow

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -10,6 +10,8 @@ async function bootstrap() {
       'https://studio.apollographql.com',
       'http://localhost:3001',
     ],
+    methods: 'POST',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
   })
   await app.listen(3000)
 }
