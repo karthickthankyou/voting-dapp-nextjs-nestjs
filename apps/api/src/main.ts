2854b6c815d1426e7aaef06d58a9739029929c1a
Karthick Ragavendran <karthickthankyou@gmail.com>
2023-07-25 21:24:15 +0530

    update port in main

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,27 +1,21 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
+const port = process.env.PORT || 3000
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',')
+
+  console.log(allowedOrigins)
+
   app.enableCors({
-    origin: [
-      'https://voting-dapp-nextjs-nestjs-web.vercel.app',
-      'https://personalities.iamkarthick.com',
-      'https://studio.apollographql.com',
-      'http://localhost:3001',
-    ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: 'Content-Type, Accept, Authorization',
+    origin: allowedOrigins,
+    allowedHeaders: '*',
+    methods: '*',
   })
-  await app.listen(3000)
+
+  await app.listen(port, '0.0.0.0')
 }
-
-process.on('uncaughtException', (err) => {
-  console.error('Unhandled exception', err)
-})
-
-process.on('unhandledRejection', (reason) => {
-  console.error('Unhandled promise rejection', reason)
-})
-
 bootstrap()
